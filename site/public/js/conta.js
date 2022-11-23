c1847d87b4812b6468dd0523cdbf4ea58b05cd3f
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2022-11-23 13:55:11 -0300

    Modificações nos demais links, principalmente no link conta, + correções

---
+++ site/public/js/conta.js
@@ -0,0 +1,27 @@
+function home() {
+
+}
+
+function jogo() {
+
+}
+
+function descubra() {
+
+}
+
+function personagens() {
+
+}
+
+function sorteio() {
+
+}
+
+function configurações() {
+
+}
+
+function exit() {
+    window.location.href = "index.html"
+}
