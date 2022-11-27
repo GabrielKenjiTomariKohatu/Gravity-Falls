168891896c8aece7711943b07608e0ef5fad6592
GabrielKenjiTomariKohatu <gabriel.kohatu@sptech.school>
2022-11-27 15:36:36 -0300

    Atualizações

--- site/public/js/conta.js
+++ site/public/js/conta.js
@@ -1,27 +1,440 @@
+
 function home() {
+    // section_home.innerHTML = '';
+    section_home.innerHTML = `
+    <div class="display">
+    <div class="titulo-display">
+        <p>Gravity Falls</p>
+    </div>
+</div>
+<div class="conteudo-home" id="DivContent">
+    <div class="box-home">
 
+    </div>
+</div>`
 }
 
-function jogo() {
+function memoria() {
+    // section_home.innerHTML = '';
+    section_home.innerHTML = `
+    <div class="display">
+    <div class="titulo-display">
+        <p>Jogo da MEMÓRIA</p>
+    </div>
+</div>
+<div class="conteudo-home" id="DivContent">
+    <div class="box-home">
 
+    </div>
+</div>`
 }
 
-function descubra() {
+function enygma() {
+    section_home.innerHTML = `
+    <div class="display">
+    <div class="titulo-display">
+        <p>Enygma</p>
+    </div>
+</div>
+<div class="conteudo-home" id="DivContent">
+    <div class="box-home">
 
+    </div>
+</div>`
 }
 
 function personagens() {
+    section_home.innerHTML = ``
+    section_home.innerHTML =
+`
+<div class="display">
+<div class="titulo-display">
+    <p>Personagens</p>
+</div>
+</div>
+<div class="conteudo-home" id="DivContent">
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/DipperPines.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Dipper Pines</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/MabelPines.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Mabel Pines</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Waddles.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Waddles</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/StanleyPines.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Stanley Pines</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/StanfordPines.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Stanford Pines</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Soos.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Soos</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Wendy.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Wendy</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/CandyChiu.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Candy</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Grenda.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Grenda</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Fiddleford Hadron.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Velho McGucket</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/GideonGleeful.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Gideão</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/PacificaNorthwest.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Pacifica Northwest</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/RobertValentino.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Robbie</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/BillsCypher.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Bill Cipher</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/SheriffBlubs.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Sheriff Blubs</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/OficialDurland.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Oficial Durland</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/TobyDeterminado.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Toby Determinado</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/LazySusan.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Lazy Susan</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Tambry.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Tambry</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Lee.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Lee</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Nate.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Nate</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Thompson.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Thompson</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/BlendinBlenjaminBlandin.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Blendin Blenjamin Blandin</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/ManlyDan.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Manly Dan</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Abuelita.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Abuelita</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Gnomo.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Gnomos</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Gompers.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Gompers, o bode.</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/TylerCutebiker.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Tyler TylerCutebiker</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/BudGleeful.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Bud BudGleeful</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/BebeDoTempo.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Bebê do Tempo</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/AgentePowers.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Agente Powers</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/AgenteTrigger.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Agente Trigger</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Mermando.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Mermando</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/TadEstranho.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Tad Estranho</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Lolph.jpg">
+        </div>
+        <div class="nome-personagem">
+            <p>Lolph</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Dundgren.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Dundgren</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/ShandraJimenez.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Shandra Jimenez</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/MonstroDoSummerween.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Monstro do Summerween</p>
+        </div>
+    </div>
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Multi-Urso.jpeg">
+        </div>
+        <div class="nome-personagem">
+            <p>Multi-Urso</p>
+        </div>
+    </div>
+</div>
+<div class="box-personagem">
+    <div class="personagem">
+        <div class="imagem-personagem">
+            <img src="./assets/icom/Rumble.png">
+        </div>
+        <div class="nome-personagem">
+            <p>Rumble</p>
+        </div>
+    </div>
+</div>
+</div>
+`
 
 }
 
-function sorteio() {
+function diarios() {
+    section_home.innerHTML = `
+    <div class="display">
+    <div class="titulo-display">
+        <p>Diários</p>
+    </div>
+</div>
+<div class="conteudo-home" id="DivContent">
+    <div class="box-home">
 
+    </div>
+</div>`
 }
 
-function configurações() {
+function estatisticas() {
+    section_home.innerHTML = 
+`
 
+`
 }
 
 function exit() {
     window.location.href = "index.html"
-}
+}  
+
+    // section_home.style.display = "none";
+    // section_memoria.style.display = "none";
+    // section_enygma.style.display = "none";
+    // section_personagens.style.display = "none";
+    // section_diarios.style.display = "none";
+    // section_sorteio.style.display = "none";
+    // section_estatisticas.style.display = "";
